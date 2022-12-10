db3902d0d22525e824378581a78229b54ad883c0
joanjanku2000 <janku.joan4@gmail.com>
2022-12-11 00:49:50 +0100

    personal deal

--- ui.py
+++ ui.py
@@ -6,6 +6,8 @@ import os, glob
 from elasticsearch import Elasticsearch, helpers
 import docx
 import PyPDF2
+from PIL import ImageTk, Image
+import threading
 
 if os.name == 'posix':
     slash = "/" # for Linux and macOS
@@ -55,25 +57,14 @@ def getText(filename):
 
     return fullText
 
-def yield_docs(all_files, textB: tk.Text):
+def yield_docs(all_files, textB: tk.Text , index):
+    if  not index or index is None or len(index) == 0:
+        textB.insert(tk.END ,"\nNo Index Provided")
+        return
     for _id, _file in enumerate(all_files):
         textB.insert(tk.END ,"\nIndexing : " + _file)
         textB.see(tk.END)
         file_name = _file[ _file.rfind(slash)+1:]
-        # if os.path.getsize(_file) > 50000000 or file_name.lower().endswith(('jpg' , '.ico' , '.png' , '.js' ,'.zip' , '.rar' ,'.exe')) is True:
-        #  doc_source = {
-        #             "file_name": file_name,
-        #             "data": _file,
-        #             "file_path":_file
-        #         }
-        #  yield {
-        #             "_index": "chipster",
-        #             #  "_type": "some_type",
-        #             # "_id": _id + 1, # number _id for each iteration
-        #             "_source": doc_source
-        #         }
-        #  continue
-    
         
         try:    
               if file_name.lower().endswith(('.html' , '.txt' , '.php')) is True :
@@ -94,7 +85,7 @@ def yield_docs(all_files, textB: tk.Text):
                             "file_path":_file
                         }
                         yield {
-                        "_index": "chipster",
+                        "_index": index,
                         "_source": doc_source
                         }
               elif file_name.lower().endswith((".pdf")) is True :
@@ -107,7 +98,7 @@ def yield_docs(all_files, textB: tk.Text):
                             "file_path":_file
                         }
                         yield {
-                        "_index": "chipster",
+                        "_index": index,
                         "_source": doc_source
                         }   
               else:
@@ -118,7 +109,7 @@ def yield_docs(all_files, textB: tk.Text):
                 }
         
                     yield {
-                    "_index": "chipster",
+                    "_index": index,
                     #  "_type": "some_type",
                     # "_id": _id + 1, # number _id for each iteration
                     "_source": doc_source
@@ -132,41 +123,54 @@ def yield_docs(all_files, textB: tk.Text):
                     "file_path":_file
                 }
           yield {
-                    "_index": "chipster",
+                    "_index": index,
                     #  "_type": "some_type",
                     # "_id": _id + 1, # number _id for each iteration
                     "_source": doc_source
                 } 
 
 root= tk.Tk('Chipster')
-root.title("Chipster Folder Indexer ")
+root.title("ElasticSearch Folder Indexer ")
 root.resizable(False,False)
 
-canvas1 = tk.Canvas(root, width=400, height=520, relief='raised')
+canvas1 = tk.Canvas(root, width=400, height=600, relief='raised')
 
+img = ImageTk.PhotoImage(Image.open("elk.png"))
+img.width = 0.1
+img.height = 0.1
+imglabel = tk.Label(root,image=img)
+canvas1.create_window(20,30,window = imglabel)
 
-label1 = tk.Label(root, text='Chipster Indexer', background="lightblue")
+label1 = tk.Label(root, text='ElasticSearch Folder Indexer', background="lightblue")
 label1.config(font=('Serif', 14, "bold"))
-canvas1.create_window(200, 25, window=label1)
+canvas1.create_window(230, 78, window=label1)
+
+
 
 label2 = tk.Label(root, text='Enter the path of the folder to be indexed:')
 label2.config(font=('helvetica', 10))
-canvas1.create_window(200, 100, window=label2)
+canvas1.create_window(200, 140, window=label2)
 
 entry1 = tk.Entry(root , width=40) 
-canvas1.create_window(200, 140, window=entry1)
+canvas1.create_window(200, 160, window=entry1)
 
-label3 = tk.Label(root, text='Enter the URL of the elasticsearch instance:')
+label3 = tk.Label(root, text='ElasticSearch Instance (ex: http://localhost:9200):')
 label3.config(font=('helvetica', 10))
-canvas1.create_window(200, 180, window=label3)
+canvas1.create_window(200, 200, window=label3)
 
 entry2 = tk.Entry(root,width=40) 
 canvas1.create_window(200, 220, window=entry2)
 
 
+label4 = tk.Label(root, text='Index Name:')
+label4.config(font=('helvetica', 10))
+canvas1.create_window(200, 260, window=label4)
+
+entry3 = tk.Entry(root,width=40) 
+canvas1.create_window(200, 280, window=entry3)
+
 text = tk.Text(root, height=10,width=40)
-# text.config(state= tk.ENAB)
-import threading
+
 
 
 
@@ -191,7 +195,7 @@ def index():
  try:
    resp = helpers.bulk(
        client,
-       yield_docs( all_files,text )
+       yield_docs( all_files,text,entry3.get() )
    )
    text.insert (tk.END,"\nhelpers.bulk() RESPONSE:"+ str(resp))
    text.insert (tk.END,"RESPONSE TYPE:"+ str(type(resp)))
@@ -212,10 +216,10 @@ style.map('TButton', background=[('active','indianred')])
 
 button1 = ttk.Button(text='Index', command=start_combine_in_bg)
 # button1.pack()
-canvas1.create_window(200, 260, window=button1)
+canvas1.create_window(200, 340, window=button1)
 
 
-canvas1.create_window(200,380,window=text)
+canvas1.create_window(200,470,window=text)
 print("END")
 canvas1.pack()
 root.mainloop()
